3c2cdf69a57d3c19953119c39adb3e7430e14444
Baaav23 <bavlybadry123@gmail.com>
2023-09-23 02:25:20 +0300

    bottom navigatiion bar

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -20,150 +20,181 @@ class _HomePageState extends State<HomePage> {
     return DefaultTabController(
       length: 4,
       child: Scaffold(
-          appBar: AppBar(
-            backgroundColor: mobileBackgroundColor,
-            title: TextFormField(
-              decoration: const InputDecoration(
-                  prefixIconColor: Colors.white,
-                  filled: true,
-                  fillColor: mobileSearchColor,
-                  border: UnderlineInputBorder(
-                      borderSide: BorderSide.none,
-                      borderRadius: BorderRadius.all(Radius.circular(25))),
-                  prefixIcon: Icon(Icons.search),
-                  suffixIcon: Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      RotatedBox(
-                          quarterTurns: 1, child: Icon(Icons.horizontal_rule)),
-                      Padding(
-                        padding: EdgeInsets.only(right: 8.0),
-                        child: Icon(Icons.mic_none),
-                      ),
-                    ],
-                  ),
-                  suffixIconColor: Colors.white,
-                  hintStyle: TextStyle(color: Colors.white),
-                  hintText: 'Search songs , playlist and artists'),
-              cursorRadius: const Radius.circular(50),
-            ),
-            leading: IconButton(
-              icon: const Icon(Icons.settings_brightness_sharp),
-              onPressed: () => _scaffoldKey.currentState?.openDrawer(),
-            ),
-            bottom: PreferredSize(
-              preferredSize: Size.fromHeight(150),
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
+        appBar: AppBar(
+          backgroundColor: mobileBackgroundColor,
+          title: TextFormField(
+            decoration: const InputDecoration(
+                prefixIconColor: Colors.white,
+                filled: true,
+                fillColor: mobileSearchColor,
+                border: UnderlineInputBorder(
+                    borderSide: BorderSide.none,
+                    borderRadius: BorderRadius.all(Radius.circular(25))),
+                prefixIcon: Icon(Icons.search),
+                suffixIcon: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
                   children: [
-                    Row(
-                      children: [
-                        Expanded(
-                            flex: 1,
-                            child: Row(
-                              children: [
-                                Expanded(
-                                  child: Padding(
-                                    padding: const EdgeInsets.only(
-                                        right: 2.5, left: 2.5),
-                                    child: MaterialButton(
-                                      height: 85,
-                                      shape: const RoundedRectangleBorder(
-                                          borderRadius: BorderRadius.all(
-                                              Radius.circular(10))),
-                                      onPressed: () {},
-                                      color:
-                                          const Color.fromARGB(255, 77, 18, 87),
-                                      child: const Column(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.center,
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.start,
-                                        children: [
-                                          Icon(Icons.favorite),
-                                          Text("Favourites")
-                                        ],
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                                Expanded(
-                                  child: Padding(
-                                    padding: const EdgeInsets.only(
-                                        right: 2.5, left: 2.5),
-                                    child: MaterialButton(
-                                      height: 85,
-                                      shape: const RoundedRectangleBorder(
-                                          borderRadius: BorderRadius.all(
-                                              Radius.circular(10))),
-                                      onPressed: () {},
-                                      color:
-                                          const Color.fromARGB(255, 3, 53, 5),
-                                      child: const Column(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.center,
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.start,
-                                        children: [
-                                          Icon(Icons.playlist_play),
-                                          Text("Playlists")
-                                        ],
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                                Expanded(
-                                  child: Padding(
-                                    padding: const EdgeInsets.only(
-                                        right: 2.5, left: 2.5),
-                                    child: MaterialButton(
-                                      height: 85,
-                                      shape: const RoundedRectangleBorder(
-                                          borderRadius: BorderRadius.all(
-                                              Radius.circular(10))),
-                                      onPressed: () {},
-                                      color:
-                                          const Color.fromARGB(255, 120, 83, 8),
-                                      child: const Column(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.center,
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.start,
-                                        children: [
-                                          Icon(Icons.recent_actors),
-                                          Text("Recent")
-                                        ],
-                                      ),
-                                    ),
-                                  ),
-                                )
-                              ],
-                            )),
-                      ],
+                    RotatedBox(
+                        quarterTurns: 1, child: Icon(Icons.horizontal_rule)),
+                    Padding(
+                      padding: EdgeInsets.only(right: 8.0),
+                      child: Icon(Icons.mic_none),
                     ),
-                    const TabBar(tabs: [
-                      Tab(
-                        child: Text("Songs"),
-                      ),
-                      Tab(child: Text("Artists")),
-                      Tab(
-                        child: Text("Albums"),
-                      ),
-                      Tab(child: Text("Folders"))
-                    ]),
                   ],
                 ),
+                suffixIconColor: Colors.white,
+                hintStyle: TextStyle(color: Colors.white),
+                hintText: 'Search songs , playlist and artists'),
+            cursorRadius: const Radius.circular(50),
+          ),
+          leading: IconButton(
+            icon: const Icon(Icons.settings_brightness_sharp),
+            onPressed: () => _scaffoldKey.currentState?.openDrawer(),
+          ),
+          bottom: PreferredSize(
+            preferredSize: Size.fromHeight(150),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      Expanded(
+                          flex: 1,
+                          child: Row(
+                            children: [
+                              Expanded(
+                                child: Padding(
+                                  padding: const EdgeInsets.only(
+                                      right: 2.5, left: 2.5),
+                                  child: MaterialButton(
+                                    height: 85,
+                                    shape: const RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.all(
+                                            Radius.circular(10))),
+                                    onPressed: () {},
+                                    color:
+                                        const Color.fromARGB(255, 77, 18, 87),
+                                    child: const Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Icon(Icons.favorite),
+                                        Text("Favourites")
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              Expanded(
+                                child: Padding(
+                                  padding: const EdgeInsets.only(
+                                      right: 2.5, left: 2.5),
+                                  child: MaterialButton(
+                                    height: 85,
+                                    shape: const RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.all(
+                                            Radius.circular(10))),
+                                    onPressed: () {},
+                                    color: const Color.fromARGB(255, 3, 53, 5),
+                                    child: const Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Icon(Icons.playlist_play),
+                                        Text("Playlists")
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              Expanded(
+                                child: Padding(
+                                  padding: const EdgeInsets.only(
+                                      right: 2.5, left: 2.5),
+                                  child: MaterialButton(
+                                    height: 85,
+                                    shape: const RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.all(
+                                            Radius.circular(10))),
+                                    onPressed: () {},
+                                    color:
+                                        const Color.fromARGB(255, 120, 83, 8),
+                                    child: const Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Icon(Icons.recent_actors),
+                                        Text("Recent")
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              )
+                            ],
+                          )),
+                    ],
+                  ),
+                  const TabBar(tabs: [
+                    Tab(
+                      child: Text("Songs"),
+                    ),
+                    Tab(child: Text("Artists")),
+                    Tab(
+                      child: Text("Albums"),
+                    ),
+                    Tab(child: Text("Folders"))
+                  ]),
+                ],
               ),
             ),
           ),
-          drawer: const Drawer(
-            child: Text(''),
+        ),
+        drawer: const Drawer(
+          child: Text(''),
+        ),
+        body: const TabBarView(
+            children: [Songs(), Artists(), Albums(), Folders()]),
+        bottomNavigationBar: Stack(fit: StackFit.passthrough, children: [
+          Container(
+            decoration: BoxDecoration(
+                color: Colors.black,
+                borderRadius: BorderRadius.all(Radius.circular(45))),
+            padding: EdgeInsets.all(10),
+            height: 60,
+            margin: const EdgeInsets.all(10),
+            child: Row(children: [
+              CircleAvatar(
+                backgroundImage: AssetImage(
+                  "assets/song.jpeg",
+                ),
+              ),
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [Text("name"), Text("artist")],
+                ),
+              ),
+              Expanded(
+                  child: IconButton(
+                      onPressed: () {}, icon: Icon(Icons.play_arrow_rounded))),
+              Expanded(
+                child: IconButton(
+                    onPressed: () {}, icon: Icon(Icons.arrow_forward_outlined)),
+              )
+            ]),
           ),
-          body: const TabBarView(
-              children: [Songs(), Artists(), Albums(), Folders()])),
+        ]),
+      ),
     );
   }
 }
